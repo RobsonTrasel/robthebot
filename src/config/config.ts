923448cfd881fdaa6ea2021edbc380ab25020ed3
Robson Trasel <robson.trasel@gmail.com>
2023-01-10 20:48:38 -0300

    chore: config setup file

---
+++ src/config/config.ts
@@ -0,0 +1,42 @@
+import {
+  ConfigObject,
+  NotificationLanguage
+} from '@open-wa/wa-automate'
+
+const config = {
+  blockCrashLogs: false,
+	disableSpins: false,
+	hostNotificationLang: NotificationLanguage.PTBR,
+	logConsole: false,
+	viewport: {
+		width: 1920,
+		height: 1200,
+	},
+	deleteSessionDataOnLogout: false,
+	popup: 3012,
+	defaultViewport: null,
+	sessionId: 'TramontaBot',
+	headless: true,
+	multiDevice: true,
+	qrTimeout: 0,
+	authTimeout: 99999999,
+	restartOnCrash: false,
+	useChrome: true,
+	killProcessOnBrowserClose: true,
+	throwErrorOnTosBlock: false,
+	chromiumArgs: [
+		'--no-sandbox',
+		'--disable-setuid-sandbox',
+		'--aggressive-cache-discard',
+		'--disable-cache',
+		'--disable-application-cache',
+		'--disable-offline-load-stale-cache',
+		'--disk-cache-size=0',
+	],
+}
+
+export default (): ConfigObject => {
+  return {
+    ...config
+  }
+}
